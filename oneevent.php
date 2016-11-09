721ee563a9a2e9c9045161eee5f25132acc76755
Dr. Joseph Blunden <olafur.petursson@gmail.com>
2016-11-09 20:44:10 +0000

    asdfjasdfasdfadsflkasfasdf

--- oneevent.php
+++ oneevent.php
@@ -19,75 +19,78 @@
 		$comment->deleteComment($commentID);
 	}
 ?>
-<div class="oneevent-container">
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-9 p-a-3">
-			<div class="row">
-					<?php if(isset($_GET['delete']) && $_GET['delete'] == 'true') : ?>
-						<div class="alert alert-danger" role="alert">
-  						<strong>Well done!</strong> You successfully deleted the event.
-						</div>
-					<?php endif; ?>
-			<div class="event-container">
-        <h2><?php echo $eventInfo['title']; ?></h2>
-        <h3><?php echo $eventInfo['eventDate']; ?></h3>
-        <p><?php echo $eventInfo['description']; ?></p>
-        <h3><?php echo $eventInfo['author']; ?></h3>
-			</div>
+<div class="vidburdur-stakur-info-container">
+	<div class="vidburdur-stakur-info">
+		<?php echo '
+			<img class="vidburdur-stakur-mynd" src="'.$eventInfo['eventImg'].'">
+			<h2>'.$eventInfo['title'].'</h2>
+			<h4>'.$eventInfo['eventDate'].'</h4>
+			<p>'.$eventInfo['description'].'</p>
+
+		' ?>
+	</div>
+</div>
+
+	<div class="vidburdur-stakur-info-container">
+		<div class="vidburdur-stakur-info">
 			<a class="edit-button-a" href="editevent.php?edit=true&eventid=<?php echo $eventid ?>">
-				<button class="edit-button">Edit</button>
-			</a>
-			<a class="delete-button-a" href="events.php?delete=true&eventid=<?php echo $eventid ?>">
-				<button class="delete-button">Delete</button>
-			</a>
+					<button class="edit-button">Edit</button>
+				</a>
+				<a class="delete-button-a" href="events.php?delete=true&eventid=<?php echo $eventid ?>">
+					<button class="delete-button">Delete</button>
+				</a>
 		</div>
+	</div>
+		
+<div class="vidburdur-stakur-info-container-bakgrunnslaust">
+	<div class="vidburdur-stakur-info">
+		<div class="comment-post">
+			<form method='POST'>
+			<input type='hidden' name='date_time' value='".<?php date('d-m-y H:i:s'); ?>."'>
+			<textarea placeholder="Athugasemd við viðburð" name='comments'></textarea><br>
+			<button type='submit' name='commentSubmit'>Comment</button>
+			</form>
 		</div>
 	</div>
 </div>
-<div class="comment-post">
-<form method='POST'>
-		<input type='hidden' name='date_time' value='".<?php date('d-m-y H:i:s'); ?>."'>
-		<textarea name='comments'></textarea><br>
-		<button type='submit' name='commentSubmit'>Comment</button>
-</form>
-</div>
-<div class="comment-container">
-	<?php
+
+
+<?php
 		$GLOBALS['eventID'] = $_GET['eventid'];
 		$allComments = new Comment();
 		$commentArr = $allComments->getAllComments($GLOBALS['eventID']);
 		for ($i=0; $i < count($commentArr); $i++) {
 	?>
-<table class="table table-striped">
-	<tbody>
-		<div class='comment-box'>
-			<div class="upper-comment">
-				<div class="userid-comment">
-					<?php
-						echo $commentArr[$i]['userID']."<br/>";
-					?>
-				</div>
-				<div class="date-comment">
-					<?php
-						echo $commentArr[$i]['dateTime']."<br/>";
-					?>
-				</div>
-			</div>
-			<p>
-				<?php
-					echo $commentArr[$i]['comments']."<br/>";
-				?>
-			</p>
-			<div class'buttons buttons-comments'>
+
+
+
+<div class="comment-single-container">
+	<div class=".vidburdur-stakur-info-container-bakgrunnslaust">
+	<div class="userid-comment">
+		<?php
+			echo $commentArr[$i]['userID']."<br/>";
+		?>
+	</div>
+	<div class="date-comment">
+		<?php
+			echo $commentArr[$i]['dateTime']."<br/>";
+		?>
+	</div>
+	<p>
+		<?php
+			echo $commentArr[$i]['comments']."<br/>";
+		?>
+	</p>
+	<div class'buttons buttons-comments'>
 				<a class='delete-button-a' href='oneevent.php?delete=true&commentID=<?php echo $commentArr[$i]['commentID'] ?>&eventid=<?php echo $eventid ?>'>
 					<button class='delete-button'>Delete</button>
 				</a>
-			</div>
-		</div>
-	</tbody>
-</table>
+	</div>
+	</div>
+</div>
+
 <?php }?>
+
 </div>
 </div>
 
